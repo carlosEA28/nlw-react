461d2edb7ff96c050035f5f8d486b77a8b05250e
carlosEA28 <carloseaduarte28@gmail.com>
2025-02-22 12:08:42 -0300

    ui part complete

--- src/app/(home)/subcription-form.tsx
+++ src/app/(home)/subcription-form.tsx
@@ -1,31 +1,80 @@
+"use client";
+
 import { User, Mail, ArrowRight } from "lucide-react";
 import { Button } from "../../components/Button";
 import { InputRoot, InputIcon, InputFIeld } from "../../components/Input";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+
+const subscriptionSchema = z.object({
+  name: z.string().min(2, "Digite seu nome completo"),
+  email: z.string().email("Digite um email válido"),
+});
+
+type SubscriptionSchema = z.infer<typeof subscriptionSchema>;
 
 export function SubscriptionForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: zodResolver(subscriptionSchema),
+  });
+
+  function onSubscribe(data: SubscriptionSchema) {
+    console.log(data);
+  }
+
   return (
     <form
       className="bg-gray-700 border border-gray-600 rounded-2xl p-8 space-y-6 w-full md:max-w-[440px]"
-      action=""
+      onSubmit={handleSubmit(onSubscribe)}
     >
       <h2 className="font-heading font-semibold text-gray-200 text-xl">
         Inscrição
       </h2>
       <div className="space-y-3">
-        <InputRoot>
-          <InputIcon>
-            <User />
-          </InputIcon>
-          <InputFIeld type="text" placeholder="Nome Completo" />
-        </InputRoot>
+        <div className="space-y-2">
+          <InputRoot>
+            <InputIcon>
+              <User />
+            </InputIcon>
+            <InputFIeld
+              type="text"
+              placeholder="Nome Completo"
+              {...register("name")}
+            />
+          </InputRoot>
 
-        <InputRoot>
-          <InputIcon>
-            <Mail />
-          </InputIcon>
-          <InputFIeld type="Email" placeholder="E-mail" />
-        </InputRoot>
+          {errors?.name && (
+            <p className="text-danger text-xs font-semibold">
+              {errors.name.message}
+            </p>
+          )}
+        </div>
+
+        <div className="space-y-2">
+          <InputRoot>
+            <InputIcon>
+              <Mail />
+            </InputIcon>
+            <InputFIeld
+              type="Email"
+              placeholder="E-mail"
+              {...register("email")}
+            />
+          </InputRoot>
+
+          {errors?.email && (
+            <p className="text-danger text-xs font-semibold">
+              {errors.email.message}
+            </p>
+          )}
+        </div>
       </div>
+
       <Button type="submit">
         Confirmar
         <ArrowRight />
--- src/app/invite/InviteLinkInput.tsx
+++ src/app/invite/InviteLinkInput.tsx
@@ -1,18 +1,25 @@
+"use client";
+
 import { IconButton } from "@/components/IconButton";
 import { InputRoot, InputIcon, InputFIeld } from "@/components/Input";
 import { Link, Copy } from "lucide-react";
 
-export function InviteLinkInput() {
+interface inviteLinkInputProps {
+  inviteLink: string;
+}
+
+export function InviteLinkInput({ inviteLink }: inviteLinkInputProps) {
+  function copyInviteLink() {
+    navigator.clipboard.writeText(inviteLink);
+  }
+
   return (
     <InputRoot>
       <InputIcon>
         <Link className="size-5" />
       </InputIcon>
-      <InputFIeld
-        readOnly
-        defaultValue={"devstage.com/codecraft-summit-2025/1289"}
-      />
-      <IconButton className="-mr-2">
+      <InputFIeld readOnly defaultValue={inviteLink} />
+      <IconButton className="-mr-2" onClick={copyInviteLink}>
         <Copy className="size-5" />
       </IconButton>
     </InputRoot>
